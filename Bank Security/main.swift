7db6c97e227aba4fa0485133c38d86b51baa75b9
Rylie Castell <rylie.castell@gmail.com>
2025-03-04 14:21:28 +0100

    add date formatter programm

--- Bank Security/main.swift
+++ Bank Security/main.swift
@@ -34,3 +34,38 @@ print("The current balance is \(convertCurrency(exchangeRateEur)) €.")
 print("The current balance is \(convertCurrency(exchangeRateUsd)) $.")
 print("The current balance is \(convertCurrency(exchangeRateGbp)) £.")
 print("The current balance is \(convertCurrency(exchangeRateJpy)) ¥.")
+
+
+// Aufgabe 2.6 Datum abspeichern
+// 2.6.1. Variablen definieren
+
+let day: Int = 21
+let month: Int = 1
+let year: Int = 1999
+
+// 2.6.2. DateComponents verwenden
+
+var dateComponents = DateComponents(year: year, month: month, day: day)
+print(dateComponents)
+
+// 2.6.3 Datum generieren
+
+let calendar = Calendar.current
+let currentDate = Date()
+dateComponents = calendar.dateComponents([.year, .month, .day], from: currentDate)
+
+print(dateComponents)
+
+// 2.6.4 Datum formatieren
+
+let dateFormatter = DateFormatter()
+dateFormatter.dateStyle = .medium
+dateFormatter.timeStyle = .none
+
+var formattedDate = dateFormatter.string(from: currentDate)
+print(formattedDate)
+
+dateFormatter.dateStyle = .long
+dateFormatter.timeStyle = .medium
+formattedDate = dateFormatter.string(from: currentDate)
+print(formattedDate)
