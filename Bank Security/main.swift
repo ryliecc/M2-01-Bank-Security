58ec25d012efec3d177c1c830846481fa02860cb
Rylie Castell <rylie.castell@gmail.com>
2025-03-04 13:14:30 +0100

    add terminator and separator

--- Bank Security/main.swift
+++ Bank Security/main.swift
@@ -2,3 +2,28 @@ import Foundation
 
 print("Hello, World!")
 
+
+// Aufgabe 2.4 Recherche über print()
+
+// 1. Verständnisfrage: Was bedeutet separator und wie kannst du einen eigenen schreiben? Sorge zB dafür dass beim Aufruf von print("Hallo","Welt") nicht Hallo Welt ausgedruckt wird, sondern zB Hallo... Welt
+
+print("Hallo","Welt", separator: "... ")
+
+/* 2. Verständnisfrage:     Was bedeutet terminator und wie kannst du einen eigenen schreiben? Sorge zB dafür, dass beim Aufruf von
+
+print("Hallo")
+print("Welt")
+ 
+nicht
+
+Hallo
+Welt
+ 
+sondern zB
+ 
+Hallo Welt
+
+ gedruckt wird. Dazu müssen die print-Aufrufe etwas verändert werden. */
+
+print("Hallo", terminator: " ")
+print("Welt")
