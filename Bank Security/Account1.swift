f6532ebc5cbf8a4039eae75ee555f4bae86119ab
Rylie Castell <rylie.castell@gmail.com>
2025-03-04 12:37:31 +0100

    add user data

--- Bank Security/Account1.swift
+++ Bank Security/Account1.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+// Aufgabe 2.1 Nutzerdaten speichern
+
+var name: String = "Beate Beispiel"
+var age: Int = 39
+var isAdult: Bool = true
+var adressTown: String = "Berlin"
+var adressStreet: String = "Breite Straße"
+var adressHouseNumber: String = "123"
+var email: String = "beate.beispiel@example.com"
+var phoneNumber: String = "+49123456789"
