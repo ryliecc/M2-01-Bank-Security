7638fe50447b730fb233402a656dddb39c844231
Rylie Castell <rylie.castell@gmail.com>
2025-03-04 13:01:44 +0100

    add account data

--- Bank Security/Account1.swift
+++ Bank Security/Account1.swift
@@ -17,3 +17,14 @@ var adressStreet: String = "Breite Straße"
 var adressHouseNumber: String = "123"
 var email: String = "beate.beispiel@example.com"
 var phoneNumber: String = "+49123456789"
+
+
+// Aufgabe 2.2 Kontodaten
+
+var bankName: String = "Beispielbank Deutschland"
+var bankTown: String = "Berlin"
+var accountNumber: String = "12345678"
+var iban: String = "DE 1234 5678 9101 1213"
+var pin: String = "1234"
+var accountSuspended: Bool = false
+var balance: Double = 1530.40
